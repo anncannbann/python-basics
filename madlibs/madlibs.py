5dda9ef4080252a696f0a632785a25a409b64164
anncannbann <anncannbann@gmail.com>
2021-07-05 21:47:06 +0530

    Game of Madlibs

---
+++ madlibs/madlibs.py
@@ -0,0 +1,9 @@
+#Program 1 : basic implementation of string concatination to implement the game of Madlibs
+
+adj = input("Adjective")
+verb1 = input("Verb")
+verb2 =input("Verb")
+famous = input("Famous Person")
+madlib = f"Computer Programming is so {adj}! It makes me excited {verb1} Stay hydrated {verb2} like you are {famous}"
+
+print(madlib)
